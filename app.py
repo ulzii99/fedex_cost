70a6485c6e20b68fd18299a8c0db2769ab38bfb3
ulzii99 <ulzii.m99@gmail.com>
2020-11-16 17:16:09 -0500

    app3

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+# import sklearn.model_selection as sk_ms
+# import statsmodels.formula.api as sm
+# import sklearn.metrics as sk_m
 from load_css import local_css
 import math
 import pickle
-import mpu
-from uszipcode import SearchEngine
+# import mpu
+# from uszipcode import SearchEngine
 local_css("style.css")
 
 
