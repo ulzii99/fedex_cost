1798ac35ca6a8e9ddb02a44e8219228f9d3eb015
ulzii99 <ulzii.m99@gmail.com>
2020-11-16 17:09:24 -0500

    clean

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn.model_selection as sk_ms
-import statsmodels.formula.api as sm
-import sklearn.metrics as sk_m
+
 from load_css import local_css
 import math
 import pickle
